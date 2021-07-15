c1b8463ed0682d3d6abe598bf450162849befc52
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-07-15 13:46:49 +0530

    server complete

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
-const express = require('express')
-const socketio = require('socket.io')
-const http = require('http')
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
-const {
-    saveUser,
-    deleteUser,
-    getUser,
-    getUsersInChat
-} = require('./controller/User')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000;
 
-const router = require('./router/UserRoute')
-const { use } = require('./router/UserRoute')
+const router = require('./router');
+const { use } = require('./router');
 
 const app = express();
-const server = http.createServer(app)
-const io = socketio(server)
+const server = http.createServer(app);
+const io = socketio(server);
 
-io.on('connection', (socketio) => {
-    console.log('New Connection . . . !')
+io.on('connection', (socket) => {
+    console.log('We Have a New Connection.......');
 
-})
+    socket.on('join', ({ name, room }, callback) => {
+       const { error, user } = addUser({ id: socket.id, name, room });
+       if(error) return callback(error);
+
+       socket.emit('message', {text: `${user.name}, Welcome to the Ax-C Chat ${user.room}`});
+       socket.broadcast.to(user.room).emit('message', {text: `${user.name}, has joined Ax-C Chat `});
+
+       socket.join(user.room);
+
+       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)})
+
+       callback();
+    });
+
+    socket.on('sendMessage', (message, callback) =>{
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text:message});
+        io.to(user.room).emit('roomdata', { room: user.room, users: getUsersInRoom(user.room)});
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('message', { text: `${user.name} has left Ax-C Chat` });
+        }
+    })
+});
+
+
+
+app.use(router);
+
+server.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`)
+});
+
--- router.js
+++ router.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/', (
-    req, res) => {
-    res.send('Ax-C Application !')
+router.get('/', (req, res) => {
+    res.send('Hello World!')
 });
 
 module.exports = router;
